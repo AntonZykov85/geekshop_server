b17bbc8d1b7aae30cede6ff58c6ee4d1cecb94fa
AntonZykov85 <zakupki_pomosh@mail.ru>
2021-10-16 15:54:59 +0300

    Lesson_3 done

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from products.models import ProductCategory, Products
+
+admin.site.register(ProductCategory)
+admin.site.register(Products)
 
 # Register your models here.
